5b989a77d840f584324a8b221afafe01f6c7c27a
ssimiao <sssimiao10@gmail.com>
2023-04-01 10:57:38 -0300

    feat: endpoints mockados

--- src/main/java/io/bookquest/entrypoint/v1/BookController.java
+++ src/main/java/io/bookquest/entrypoint/v1/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
@@ -37,6 +38,37 @@ public class BookController {
                                                  @RequestBody ReadingEntrypoint reading) {
         String[] idComposable = externalId.split("-");
         bookService.updateReading(idComposable[0], idComposable[1], reading);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/books/{idBook}/forums")
+    public Object getForum() {
+        return Map.of("discussion", "o que vocês sentiram falta nesse livro?", "comments",
+                List.of(Map.of("name","Killua", "comment", "senti falta de mais presença dos anões nas batalhas","upvote", 3, "downvote", 0)));
+    }
+
+    @PostMapping("/books/{idBook}/forums")
+    public Object createForum() {
+        return ResponseEntity.created(URI.create("/")).build();
+    }
+
+    @PutMapping("/books/{idBook}/forums/{idForum}")
+    public Object updateForum() {
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/books/{idBook}/forums/{idForum}/comments")
+    public Object createComment() {
+        return ResponseEntity.created(URI.create("/")).build();
+    }
+
+    @PutMapping("/books/{idBook}/forums/{idForum}/comments/{idComment}")
+    public Object updateComment() {
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/books/{idBook}/forums/{idForum}/comments/{idComment}")
+    public Object upvoteDownVoteComment() {
         return ResponseEntity.noContent().build();
     }
 }
