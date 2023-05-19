1268e4ff9c1955b550ea8bf1f672fc641f6b0b43
Samuel Simião <67723030+ssimiao@users.noreply.github.com>
2023-05-18 22:48:27 -0300

    Update JwtAuthFilter.java

--- src/main/java/io/bookquest/config/security/JwtAuthFilter.java
+++ src/main/java/io/bookquest/config/security/JwtAuthFilter.java
@@ -33,8 +33,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         var jsonRequest = new HashMap<>();
         jsonRequest.put("auth", authHeader);
-        jsonRequest.put("content-type",request.getContentType());
-        if (request.getContentType().contains("multipart")) {
+        jsonRequest.put("content-type", request.getContentType());
+        if (request.getContentType() != null && request.getContentType().contains("multipart")) {
             jsonRequest.put("content-type", request.getContentType());
             String requestJson = new ObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false).writeValueAsString(request.getParts());
             jsonRequest.put("request", requestJson);
